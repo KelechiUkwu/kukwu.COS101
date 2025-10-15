b3c67fe689bfb53d02563a6b8f76bbe4af1874f5
KelechiUkwu <kelechi.ukwu@pau.edu.ng>
2025-10-15 19:49:25 +0100

    week 3

--- main.rs
+++ main.rs
@@ -1,9 +1,4 @@
 fn main() {
-    let result =10.00;           //f64 by default
-    let interest:f32 =8.35;
-    let cost:f64 = 15_000.00;   //double precision
-    
-    println!("result value is {}",result);
-    println!("=interest is {}",interest);
-    println!("cost is {}",cost);
+    let isgood: bool = true;
+    println!("PAU is a good university? {}", isgood);
 }
