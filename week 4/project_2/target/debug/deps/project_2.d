3c1441573dad493d040505a5a3250dba2e4bd412
kukwu.COS101 <kelechi.ukwu@pau.edu.ng>
2025-10-25 22:02:25 +0100

    (My week 4 project2 commit)

---
+++ week 4/project_2/target/debug/deps/project_2.d
@@ -0,0 +1,5 @@
+C:\Users\kk\Desktop\kukwu.COS101\week 4\project_2\target\debug\deps\project_2.d: src\main.rs
+
+C:\Users\kk\Desktop\kukwu.COS101\week 4\project_2\target\debug\deps\project_2.exe: src\main.rs
+
+src\main.rs:
---
+++ week 4/project_2/target/debug/project_2.d
@@ -0,0 +1,1 @@
+C:\Users\kk\Desktop\kukwu.COS101\week\ 4\project_2\target\debug\project_2.exe: C:\Users\kk\Desktop\kukwu.COS101\week\ 4\project_2\src\main.rs
